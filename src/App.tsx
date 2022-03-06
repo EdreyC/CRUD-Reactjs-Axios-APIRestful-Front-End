adc5aeae7d778701b47044244bc3393c5964d0f1
EdreyC <edreycarneiro.profissional@gmail.com>
2022-03-06 12:18:59 -0300

    ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import AiOutlineDelete from 'react-icons/ai'
+import "../src/styles/home.scss"
+
 
 type Categorys = {
   name: string,
@@ -12,61 +15,69 @@ function App() {
   const [inputName, setinputName] = useState("")
   const [inputDescription, setinputDescription] = useState("")
 
-  const url="http://localhost:3002/categories"
-  
-  async function get(){
-    axios.get(url)
-    .then(res => {
-      setData(res.data)
-      console.log(data)
-    })
+  const url = "http://localhost:3002/categories"
+
+  async function get() {
+    await axios.get(url)
+      .then(res => {
+        setData(res.data)
+        console.log(data)
+      })
   }
 
   async function create() {
-    await axios.post(url,{
-      name:inputName,
-      description:inputDescription
+    await axios.post(url, {
+      name: inputName,
+      description: inputDescription
     })
     get();
   }
+
+ 
   useEffect(() => {
-   get();
+    get();
   }, []);
-  
+
   return (
+
     <div className="App">
-      <input 
-      value={inputName}
-      onChange={(event)=>{
-        setinputName(event.target.value);
-      }}
-      />
-      <input 
-      value={inputDescription}
-      onChange={(event)=>{
-        setinputDescription(event.target.value);
-      }}
-      />
-      <button
-      onClick={create}
-      >
-        Enviar
-      </button>
-      <ul>
+
+      <div className='created-category'>
+          <input
+            value={inputName}
+            onChange={
+              (event) => {
+                setinputName(event.target.value)
+                console.log(inputName);
+              }
+            }
+          />
+          <textarea
+            value={inputDescription}
+            onChange={(event) => {
+              setinputDescription(event.target.value);
+            }}
+          />
+          <button
+            onClick={create}
+          >
+            Enviar
+          </button>
+          <ul>
         {
           data.map(item => {
             return (
-              <>
-              <li>{item.name}<br />
-                <span>{item.description}</span><br />
-                <span>{item.created_at}</span>
-              </li>
-              
-              </>
+              <div>
+                <h4>{item.name}</h4>
+                <p>{item.description}</p>
+              </div>
             )
           })
         }
       </ul>
+      </div>
+      
+     
     </div>
   )
 }
